d8784096eb107558a25425e159e0cbd49f676334
Bill Burke <bburke@redhat.com>
2004-10-05 23:30:59 +0000

    tutorial
    git-svn-id: https://svn.jboss.org/repos/jbossas/trunk/jboss-ejb3/docs/tutorial/joininheritance@24283 84be2c1e-ba19-0410-b317-a758671a6fc1

---
+++ src/org/jboss/tutorial/joininheritance/Client/Client.java
@@ -0,0 +1,40 @@
+/*
+ * JBoss, the OpenSource J2EE webOS
+ *
+ * Distributable under LGPL license.
+ * See terms of license at gnu.org.
+ */
+package org.jboss.tutorial.joininheritance.Client;
+
+import org.jboss.tutorial.joininheritance.bean.Pet;
+import org.jboss.tutorial.joininheritance.bean.PetDAO;
+
+import javax.naming.InitialContext;
+
+import java.util.List;
+
+/**
+ * Comment
+ *
+ * @author <a href="mailto:bill@jboss.org">Bill Burke</a>
+ * @version $Revision$
+ */
+public class Client
+{
+   public static void main(String[] args) throws Exception
+   {
+      InitialContext ctx = new InitialContext();
+      PetDAO dao = (PetDAO) ctx.lookup(PetDAO.class.getName());
+
+      dao.createCat("Toonses", 15.0, 9);
+      dao.createCat("Sox", 10.0, 5);
+      dao.createDog("Winnie", 70.0, 5);
+      dao.createDog("Junior", 11.0, 1);
+
+      List l = dao.findByWeight(14.0);
+      for (Object o : l)
+      {
+         System.out.println(((Pet) o).getName());
+      }
+   }
+}
---
+++ src/org/jboss/tutorial/joininheritance/bean/Cat.java
@@ -0,0 +1,29 @@
+/*
+ * JBoss, the OpenSource J2EE webOS
+ *
+ * Distributable under LGPL license.
+ * See terms of license at gnu.org.
+ */
+package org.jboss.tutorial.joininheritance.bean;
+
+import javax.ejb.DiscriminatorType;
+import javax.ejb.Entity;
+import javax.ejb.Inheritance;
+import javax.ejb.InheritanceType;
+
+@Entity
+        @Inheritance(strategy = InheritanceType.JOINED, discriminatorType = DiscriminatorType.STRING, discriminatorValue = "CAT")
+        public class Cat extends Pet
+{
+   int lives;
+
+   public int getLives()
+   {
+      return lives;
+   }
+
+   public void setLives(int lives)
+   {
+      this.lives = lives;
+   }
+}
