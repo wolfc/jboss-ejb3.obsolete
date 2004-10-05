8dc66400922efcb2c54b86805342c89a847c92ed
Bill Burke <bburke@redhat.com>
2004-10-05 23:30:59 +0000

    tutorial
    git-svn-id: https://svn.jboss.org/repos/jbossas/trunk/jboss-ejb3/docs/tutorial/merge@24283 84be2c1e-ba19-0410-b317-a758671a6fc1

---
+++ src/org/jboss/tutorial/merge/bean/CustomerDAOBean.java
@@ -0,0 +1,41 @@
+/*
+ * JBoss, the OpenSource J2EE webOS
+ *
+ * Distributable under LGPL license.
+ * See terms of license at gnu.org.
+ */
+package org.jboss.tutorial.merge.bean;
+
+import javax.ejb.EntityManager;
+import javax.ejb.Inject;
+
+import java.util.List;
+
+public class CustomerDAOBean implements CustomerDAO
+{
+   @Inject
+           private EntityManager manager;
+
+
+   public int create(String first, String last, String street, String city, String state, String zip)
+   {
+      Customer customer = new Customer(first, last, street, city, state, zip);
+      manager.create(customer);
+      return customer.getId();
+   }
+
+   public Customer find(int id)
+   {
+      return manager.find(Customer.class, id);
+   }
+
+   public List findByLastName(String name)
+   {
+      return manager.createQuery("from Customer c where c.last = :name").setParameter("name", name).listResults();
+   }
+
+   public void merge(Customer c)
+   {
+      manager.merge(c);
+   }
+}
