dc48f8befea728b351e2e5b61dbd60ba749405ff
Bill Burke <bburke@redhat.com>
2005-01-26 23:25:00 +0000

    update to match specification
    git-svn-id: https://svn.jboss.org/repos/jbossas/trunk/jboss-ejb3/docs/tutorial/mdb@27992 84be2c1e-ba19-0410-b317-a758671a6fc1

--- src/org/jboss/tutorial/mdb/bean/ExampleMDB.java
+++ src/org/jboss/tutorial/mdb/bean/ExampleMDB.java
@@ -13,8 +13,8 @@ import javax.jms.MessageListener;
 
 @MessageDriven(activateConfig =
         {
-        @ActivationConfigProperty(name="destinationType", value="javax.jms.Queue"),
-        @ActivationConfigProperty(name="destination", value="queue/tutorial/example")
+        @ActivationConfigProperty(propertyName="destinationType", propertyValue="javax.jms.Queue"),
+        @ActivationConfigProperty(propertyName="destination", propertyValue="queue/tutorial/example")
         })
 public class ExampleMDB implements MessageListener
 {
