dd7e91a4660ffe0cbae7897e94079b005f217c26
Bill Burke <bburke@redhat.com>
2005-05-31 20:15:47 +0000

    move annotations to common package structure Remove @Inject annotation from javax.ejb @Resource is now used instead of @Inject
    git-svn-id: https://svn.jboss.org/repos/jbossas/trunk/jboss-ejb3/docs/tutorial/relationships@31689 84be2c1e-ba19-0410-b317-a758671a6fc1

--- src/org/jboss/tutorial/relationships/bean/EntityTestBean.java
+++ src/org/jboss/tutorial/relationships/bean/EntityTestBean.java
@@ -8,7 +8,7 @@ package org.jboss.tutorial.relationships.bean;
 
 import java.util.HashSet;
 import java.util.Set;
-import javax.ejb.Inject;
+import javax.annotation.Resource;
 import javax.ejb.RemoteInterface;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
@@ -23,7 +23,7 @@ import javax.persistence.EntityManager;
 @RemoteInterface(EntityTest.class)
 public class EntityTestBean implements EntityTest
 {
-   private @Inject EntityManager manager;
+   private @Resource EntityManager manager;
 
    public void manyToManyCreate() throws Exception
    {
