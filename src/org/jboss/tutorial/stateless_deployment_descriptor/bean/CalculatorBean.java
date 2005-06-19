3688153951747b814ea8a83972a0918c139a63cd
Bill Burke <bburke@redhat.com>
2005-06-19 01:40:12 +0000

    rename Back to @Remote/@Local from @RemoteInterface/@LocalInterface
    git-svn-id: https://svn.jboss.org/repos/jbossas/trunk/jboss-ejb3/docs/tutorial/stateless_deployment_descriptor@32473 84be2c1e-ba19-0410-b317-a758671a6fc1

--- src/org/jboss/tutorial/stateless_deployment_descriptor/bean/CalculatorBean.java
+++ src/org/jboss/tutorial/stateless_deployment_descriptor/bean/CalculatorBean.java
@@ -6,10 +6,6 @@
  */
 package org.jboss.tutorial.stateless_deployment_descriptor.bean;
 
-import javax.ejb.Stateless;
-import javax.ejb.RemoteInterface;
-import javax.ejb.LocalInterface;
-   
 public class CalculatorBean implements CalculatorRemote, CalculatorLocal
 {
    public int add(int x, int y)
