1efb9a86a55bf8e8f5d7ca07e980cb8d536837dc
Bill Burke <bburke@redhat.com>
2005-06-24 04:49:29 +0000

    update
    git-svn-id: https://svn.jboss.org/repos/jbossas/trunk/jboss-ejb3/docs/tutorial/security@32809 84be2c1e-ba19-0410-b317-a758671a6fc1

--- src/org/jboss/tutorial/security/client/Client.java
+++ src/org/jboss/tutorial/security/client/Client.java
@@ -1,40 +1,51 @@
 /*
- * JBoss, the OpenSource J2EE webOS
+ * JBoss, Home of Professional Open Source
  *
  * Distributable under LGPL license.
  * See terms of license at gnu.org.
  */
 package org.jboss.tutorial.security.client;
 
+import java.util.Properties;
+import javax.ejb.EJBAccessException;
+import javax.naming.Context;
 import javax.naming.InitialContext;
-import org.jboss.security.SecurityAssociation;
-import org.jboss.security.SimplePrincipal;
 import org.jboss.tutorial.security.bean.Calculator;
 
+/**
+ * @version $Revision$
+ */
 public class Client
 {
    public static void main(String[] args) throws Exception
    {
-      InitialContext ctx = new InitialContext();
+      // Establish the proxy with an incorrect security identity
+      Properties env = new Properties();
+      env.setProperty(Context.SECURITY_PRINCIPAL, "kabir");
+      env.setProperty(Context.SECURITY_CREDENTIALS, "invalidpassword");
+      env.setProperty(Context.INITIAL_CONTEXT_FACTORY, "org.jboss.security.jndi.JndiLoginInitialContextFactory");
+      InitialContext ctx = new InitialContext(env);
       Calculator calculator = (Calculator) ctx.lookup(Calculator.class.getName());
-
-      SecurityAssociation.setPrincipal(new SimplePrincipal("kabir"));
 
       System.out.println("Kabir is a student.");
       System.out.println("Kabir types in the wrong password");
-      SecurityAssociation.setCredential("invalidpassword".toCharArray());
       try
       {
          System.out.println("1 + 1 = " + calculator.add(1, 1));
       }
-      catch (javax.ejb.EJBAccessException ex)
+      catch (EJBAccessException ex)
       {
-         System.out.println(ex.getMessage());
+         System.out.println("Saw expected SecurityException: " + ex.getMessage());
       }
 
       System.out.println("Kabir types in correct password.");
       System.out.println("Kabir does unchecked addition.");
-      SecurityAssociation.setCredential("validpassword".toCharArray());
+
+      // Re-establish the proxy with the correct security identity
+      env.setProperty(Context.SECURITY_CREDENTIALS, "validpassword");
+      ctx = new InitialContext(env);
+      calculator = (Calculator) ctx.lookup(Calculator.class.getName());
+
       System.out.println("1 + 1 = " + calculator.add(1, 1));
 
       System.out.println("Kabir is not a teacher so he cannot do division");
