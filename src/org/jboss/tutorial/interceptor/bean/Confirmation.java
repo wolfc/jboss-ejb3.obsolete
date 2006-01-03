16cd6f00597666873148197308015ae94a3a275e
Bill Burke <bburke@redhat.com>
2006-01-03 04:01:00 +0000

    update to latest HEM that has GeneratedValue change
    git-svn-id: https://svn.jboss.org/repos/jbossas/trunk/jboss-ejb3/docs/tutorial/interceptor@39429 84be2c1e-ba19-0410-b317-a758671a6fc1

--- src/org/jboss/tutorial/interceptor/bean/Confirmation.java
+++ src/org/jboss/tutorial/interceptor/bean/Confirmation.java
@@ -24,7 +24,7 @@ package org.jboss.tutorial.interceptor.bean;
 import java.util.Date;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratorType;
+import javax.persistence.GeneratedValue; import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 /**
