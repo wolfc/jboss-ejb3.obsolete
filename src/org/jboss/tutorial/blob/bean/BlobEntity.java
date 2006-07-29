7323ba459b619a497e84f6a9028635b0a129aeee
root <no-reply@redhat.com>
2006-07-29 23:30:52 +0000

    test
    git-svn-id: https://svn.jboss.org/repos/jbossas/mutated/trunk/ejb3/docs/tutorial/blob@53674 84be2c1e-ba19-0410-b317-a758671a6fc1

--- src/org/jboss/tutorial/blob/bean/BlobEntity.java
+++ src/org/jboss/tutorial/blob/bean/BlobEntity.java
@@ -1,24 +1,36 @@
 /*
- * JBoss, the OpenSource J2EE webOS
- *
- * Distributable under LGPL license.
- * See terms of license at gnu.org.
- */
+  * JBoss, Home of Professional Open Source
+  * Copyright 2005, JBoss Inc., and individual contributors as indicated
+  * by the @authors tag. See the copyright.txt in the distribution for a
+  * full listing of individual contributors.
+  *
+  * This is free software; you can redistribute it and/or modify it
+  * under the terms of the GNU Lesser General Public License as
+  * published by the Free Software Foundation; either version 2.1 of
+  * the License, or (at your option) any later version.
+  *
+  * This software is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+  * Lesser General Public License for more details.
+  *
+  * You should have received a copy of the GNU Lesser General Public
+  * License along with this software; if not, write to the Free
+  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+  */
 package org.jboss.tutorial.blob.bean;
-
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratorType;
-import javax.persistence.Id;
-import javax.persistence.Lob;
-import javax.persistence.LobType;
-import javax.persistence.Id;
-import javax.persistence.Lob;
-import javax.persistence.FetchType;
 
 import java.io.Serializable;
 import java.sql.Blob;
 import java.sql.Clob;
+import javax.persistence.Basic;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
 
 /**
  * Comment
@@ -33,7 +45,7 @@ public class BlobEntity implements Serializable
    private Blob blobby;
    private Clob clobby;
 
-   @Id(generate = GeneratorType.IDENTITY)
+   @Id @GeneratedValue(strategy=GenerationType.IDENTITY)
    public long getId()
    {
       return id;
@@ -44,7 +56,7 @@ public class BlobEntity implements Serializable
       this.id = id;
    }
 
-   @Lob(fetch = FetchType.EAGER)
+   @Lob @Basic(fetch = FetchType.EAGER)
    public Blob getBlobby()
    {
       return blobby;
@@ -55,7 +67,7 @@ public class BlobEntity implements Serializable
       this.blobby = blobby;
    }
 
-   @Lob(type = LobType.CLOB, fetch = FetchType.EAGER)
+   @Lob @Basic(fetch = FetchType.EAGER)
    public Clob getClobby()
    {
       return clobby;
--- src/org/jboss/tutorial/blob/bean/BlobEntity2.java
+++ src/org/jboss/tutorial/blob/bean/BlobEntity2.java
@@ -1,18 +1,34 @@
 /*
- * JBoss, the OpenSource J2EE webOS
- *
- * Distributable under LGPL license.
- * See terms of license at gnu.org.
- */
+  * JBoss, Home of Professional Open Source
+  * Copyright 2005, JBoss Inc., and individual contributors as indicated
+  * by the @authors tag. See the copyright.txt in the distribution for a
+  * full listing of individual contributors.
+  *
+  * This is free software; you can redistribute it and/or modify it
+  * under the terms of the GNU Lesser General Public License as
+  * published by the Free Software Foundation; either version 2.1 of
+  * the License, or (at your option) any later version.
+  *
+  * This software is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+  * Lesser General Public License for more details.
+  *
+  * You should have received a copy of the GNU Lesser General Public
+  * License along with this software; if not, write to the Free
+  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+  */
 package org.jboss.tutorial.blob.bean;
 
 import java.io.Serializable;
+import javax.persistence.Basic;
 import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.GeneratorType;
-import javax.persistence.Lob;
 import javax.persistence.FetchType;
-import javax.persistence.LobType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
 
 /**
  * comment
@@ -26,7 +42,7 @@ public class BlobEntity2 implements Serializable
    private byte[] blobby;
    private String clobby;
 
-   @Id(generate = GeneratorType.AUTO)
+   @Id @GeneratedValue(strategy=GenerationType.AUTO)
    public long getId()
    {
       return id;
@@ -37,7 +53,7 @@ public class BlobEntity2 implements Serializable
       this.id = id;
    }
 
-   @Lob(fetch = FetchType.EAGER)
+   @Lob @Basic(fetch = FetchType.EAGER)
    public byte[] getBlobby()
    {
       return blobby;
@@ -48,7 +64,7 @@ public class BlobEntity2 implements Serializable
       this.blobby = blobby;
    }
 
-   @Lob(type = LobType.CLOB, fetch = FetchType.EAGER)
+   @Lob @Basic(fetch = FetchType.EAGER)
    public String getClobby()
    {
       return clobby;
