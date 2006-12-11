2e84759185a380164184243b5b2c6b9503570ed2
Carlo de Wolf <cdewolf@redhat.com>
2006-12-11 13:42:04 +0000

    git-svn-id: https://svn.jboss.org/repos/jbossas/projects/ejb3/trunk@58974 84be2c1e-ba19-0410-b317-a758671a6fc1

---
+++ injection/src/main/java/org/jboss/injection/aop/ConstructorInterceptor.java
@@ -0,0 +1,46 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2006, Red Hat Middleware LLC, and individual contributors as indicated
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.jboss.injection.aop;
+
+import org.jboss.aop.advice.Interceptor;
+import org.jboss.aop.joinpoint.Invocation;
+import org.jboss.injection.InjectorProcessor;
+
+/**
+ * Intercepts construction of new objects and fires up injection.
+ *
+ * @author <a href="mailto:carlo.dewolf@jboss.com">Carlo de Wolf</a>
+ * @version $Revision: $
+ */
+public class ConstructorInterceptor implements Interceptor
+{
+   public String getName()
+   {
+      return "ConstructorInterceptor";
+   }
+
+   public Object invoke(Invocation invocation) throws Throwable
+   {
+      InjectorProcessor.process(invocation.getTargetObject());
+      return invocation.invokeNext();
+   }
+}
