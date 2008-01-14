d827a2988ae9351e5e9ac90eafb7316da83a7291
Carlo de Wolf <cdewolf@redhat.com>
2008-01-14 09:46:03 +0000

    Allow for different annotation repository
    git-svn-id: https://svn.jboss.org/repos/jbossas/projects/ejb3/trunk@68937 84be2c1e-ba19-0410-b317-a758671a6fc1

--- interceptors/src/main/java/org/jboss/ejb3/interceptors/container/ManagedObjectAdvisor.java
+++ interceptors/src/main/java/org/jboss/ejb3/interceptors/container/ManagedObjectAdvisor.java
@@ -21,6 +21,9 @@
  */
 package org.jboss.ejb3.interceptors.container;
 
+import java.lang.annotation.Annotation;
+import java.lang.reflect.AnnotatedElement;
+import java.lang.reflect.Member;
 import java.util.List;
 
 import org.jboss.aop.AspectManager;
@@ -30,10 +33,13 @@ import org.jboss.aop.InstanceAdvisor;
 import org.jboss.aop.InstanceAdvisorDelegate;
 import org.jboss.aop.advice.AspectDefinition;
 import org.jboss.aop.advice.Interceptor;
+import org.jboss.aop.annotation.AnnotationRepository;
 import org.jboss.aop.introduction.AnnotationIntroduction;
 import org.jboss.aop.joinpoint.Joinpoint;
 import org.jboss.aop.metadata.SimpleMetaData;
 import org.jboss.ejb3.interceptors.ManagedObject;
+import org.jboss.ejb3.interceptors.aop.ExtendedAdvisor;
+import org.jboss.ejb3.metadata.annotation.ExtendedAnnotationRepository;
 import org.jboss.logging.Logger;
 
 /**
@@ -42,7 +48,7 @@ import org.jboss.logging.Logger;
  * @author <a href="mailto:carlo.dewolf@jboss.com">Carlo de Wolf</a>
  * @version $Revision: $
  */
-public class ManagedObjectAdvisor<T, C extends AbstractContainer<T, C>> extends ClassAdvisor implements InstanceAdvisor
+public class ManagedObjectAdvisor<T, C extends AbstractContainer<T, C>> extends ClassAdvisor implements ExtendedAdvisor, InstanceAdvisor
 {
    private static final Logger log = Logger.getLogger(ManagedObjectAdvisor.class);
    
@@ -51,15 +57,38 @@ public class ManagedObjectAdvisor<T, C extends AbstractContainer<T, C>> extends 
    
    protected ManagedObjectAdvisor(C container, String name, AspectManager manager)
    {
+      this(container, name, manager, null);
+   }
+
+   /**
+    * 
+    * @param container
+    * @param name
+    * @param manager
+    * @param pAnnotations   an alternate annotation repository or null for the default
+    */
+   protected ManagedObjectAdvisor(C container, String name, AspectManager manager, AnnotationRepository pAnnotations)
+   {
       super(name, manager);
+      
       assert container != null : "container is null";
       
       this.container = container;
       
+      if(pAnnotations != null)
+         this.annotations = pAnnotations;
+      
       // For convenience we add the ManagedObject annotation
-      annotations.addClassAnnotation(ManagedObject.class, new Object());
+      ManagedObject annotation = new ManagedObject()
+      {
+         public Class<? extends Annotation> annotationType()
+         {
+            return ManagedObject.class;
+         }
+      };
+      annotations.addClassAnnotation(ManagedObject.class, annotation);
    }
-
+   
    private void deployAnnotationIntroduction(AnnotationIntroduction introduction)
    {
       log.debug("deploy annotation introduction " + introduction);
@@ -189,5 +218,60 @@ public class ManagedObjectAdvisor<T, C extends AbstractContainer<T, C>> extends 
    public void removeInterceptorStack(String name)
    {
       throw new RuntimeException("NYI");
+   }
+   
+   /* ExtendedAdvisor */
+
+   public boolean isAnnotationPresent(Class<?> cls, Class<? extends Annotation> annotationType)
+   {
+      if(annotations instanceof ExtendedAnnotationRepository)
+      {
+         // TODO: disabled?
+         if(((ExtendedAnnotationRepository) annotations).hasAnnotation(cls, annotationType))
+            return true;
+      }
+      return cls.isAnnotationPresent(annotationType);
+   }
+
+   public boolean isAnnotationPresent(Class<?> cls, Member member, Class<? extends Annotation> annotationType)
+   {
+      if(annotations instanceof ExtendedAnnotationRepository)
+      {
+         // TODO: disabled?
+         if(((ExtendedAnnotationRepository) annotations).hasAnnotation(cls, member, annotationType))
+            return true;
+      }
+      if(member instanceof AnnotatedElement)
+      {
+         if(((AnnotatedElement) member).isAnnotationPresent(annotationType))
+            return true;
+      }
+      return false;
+   }
+
+   public <A extends Annotation> A resolveAnnotation(Class<?> cls, Class<A> annotationType)
+   {
+      A annotation = null;
+      if(annotations instanceof ExtendedAnnotationRepository)
+      {
+         // TODO: disabled?
+         annotation = ((ExtendedAnnotationRepository) annotations).resolveAnnotation(cls, annotationType);
+      }
+      if(annotation == null)
+         annotation = cls.getAnnotation(annotationType);
+      return annotation;
+   }
+
+   public <A extends Annotation> A resolveAnnotation(Class<?> cls, Member member, Class<A> annotationType)
+   {
+      A annotation = null;
+      if(annotations instanceof ExtendedAnnotationRepository)
+      {
+         // TODO: disabled?
+         annotation = ((ExtendedAnnotationRepository) annotations).resolveAnnotation(cls, member, annotationType);
+      }
+      if(annotation == null && member instanceof AnnotatedElement)
+         annotation = ((AnnotatedElement) member).getAnnotation(annotationType);
+      return annotation;
    }
 }
