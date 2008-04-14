aaa031aef1b452c543917ea093cd0073d1c584ff
Kabir Khan <kkhan@redhat.com>
2008-04-14 09:14:36 +0000

    [EJBTHREE-1275]Set the InstanceAdvisorDelegate in the ManagedObjectAdvisor from the ManagedObjectAdvisorInstanceInterceptor
    git-svn-id: https://svn.jboss.org/repos/jbossas/projects/ejb3/trunk@72154 84be2c1e-ba19-0410-b317-a758671a6fc1

--- interceptors/src/test/java/org/jboss/ejb3/test/interceptors/proxyinstanceadvisor/unit/ProxyInstanceAdvisorTestCase.java
+++ interceptors/src/test/java/org/jboss/ejb3/test/interceptors/proxyinstanceadvisor/unit/ProxyInstanceAdvisorTestCase.java
@@ -21,17 +21,13 @@
  */
 package org.jboss.ejb3.test.interceptors.proxyinstanceadvisor.unit;
 
-import java.net.URL;
-
 import junit.framework.TestCase;
 
-import org.jboss.aop.AspectXmlLoader;
-import org.jboss.ejb3.interceptors.proxy.ProxyContainer;
+import org.jboss.ejb3.test.interceptors.common.AOPDeployer;
 import org.jboss.ejb3.test.interceptors.proxyinstanceadvisor.MyInterface;
 import org.jboss.ejb3.test.interceptors.proxyinstanceadvisor.PerInstanceInterceptor;
 import org.jboss.ejb3.test.interceptors.proxyinstanceadvisor.PerJoinpointInterceptor;
 import org.jboss.ejb3.test.interceptors.proxyinstanceadvisor.ProxiedBean;
-import org.jboss.ejb3.test.interceptors.proxyinstanceadvisor.ProxiedInterceptor;
 import org.jboss.ejb3.test.interceptors.proxyinstanceadvisor.ProxyContainerWithPool;
 import org.jboss.ejb3.test.interceptors.proxyinstanceadvisor.SimplePoolInterceptor;
 import org.jboss.logging.Logger;
@@ -48,50 +44,59 @@ public class ProxyInstanceAdvisorTestCase extends TestCase
 
    public void test1() throws Throwable
    {
+      log.info("======= ProxyInstanceAdvisor.test1()");
       //AspectManager.verbose = true;
       
-      // Bootstrap AOP
-      URL url = Thread.currentThread().getContextClassLoader().getResource("proxyinstanceadvisor/jboss-aop.xml");
-      log.info("deploying AOP from " + url);
-      AspectXmlLoader.deployXML(url);
-
-      Thread.currentThread().setContextClassLoader(MyInterface.class.getClassLoader());
-      
-      ProxyContainerWithPool<ProxiedBean> container = new ProxyContainerWithPool<ProxiedBean>("ProxyTestCase", "InterceptorContainer", ProxiedBean.class);
-      
-      
-      Class<?> interfaces[] = { MyInterface.class };
-      MyInterface proxy = container.constructProxy(interfaces);
-      
-      reset(true);
-      String result = proxy.sayHi("Me");
-      assertEquals("Hi Me", result);
-      ProxiedBean bean1hi = ProxiedBean.instance;
-      PerInstanceInterceptor pi1hi = PerInstanceInterceptor.instance;
-      PerJoinpointInterceptor pj1hi = PerJoinpointInterceptor.instance;
-      
-      reset(false);
-      result = proxy.sayBye("Me");
-      assertEquals("Bye Me", result);
-      ProxiedBean bean1bye = ProxiedBean.instance;
-      PerInstanceInterceptor pi1bye = PerInstanceInterceptor.instance;
-      PerJoinpointInterceptor pj1bye = PerJoinpointInterceptor.instance;
-      
-      assertSame(bean1hi, bean1bye);
-      assertSame(pi1hi, pi1bye);
-      assertNotSame(pj1hi, pj1bye);
-
-      reset(true);
-      result = proxy.sayHi("Me");
-      assertEquals("Hi Me", result);
-      ProxiedBean bean2hi = ProxiedBean.instance;
-      PerInstanceInterceptor pi2hi = PerInstanceInterceptor.instance;
-      PerJoinpointInterceptor pj2hi = PerJoinpointInterceptor.instance;
-      assertNotSame(bean1hi, bean2hi);
-      
-      //FIXME - These must be enabled to start the test
-//      assertNotSame(pi2hi, pi1hi);
-//      assertNotSame(pj2hi, pj1hi);
+      AOPDeployer deployer = new AOPDeployer("proxyinstanceadvisor/jboss-aop.xml");
+      try
+      {
+         // Bootstrap AOP
+         log.info(deployer.deploy());
+   
+         Thread.currentThread().setContextClassLoader(MyInterface.class.getClassLoader());
+         
+         ProxyContainerWithPool<ProxiedBean> container = new ProxyContainerWithPool<ProxiedBean>("ProxyInstanceAdvisorTestCase", "InterceptorContainer", ProxiedBean.class);
+         
+         
+         Class<?> interfaces[] = { MyInterface.class };
+         MyInterface proxy = container.constructProxy(interfaces);
+         
+         
+         reset(true);
+         String result = proxy.sayHi("Me");
+         assertEquals("Hi Me", result);
+         ProxiedBean bean1hi = ProxiedBean.instance;
+         PerInstanceInterceptor pi1hi = PerInstanceInterceptor.instance;
+         PerJoinpointInterceptor pj1hi = PerJoinpointInterceptor.instance;
+         
+         reset(false);
+         result = proxy.sayBye("Me");
+         assertEquals("Bye Me", result);
+         ProxiedBean bean1bye = ProxiedBean.instance;
+         PerInstanceInterceptor pi1bye = PerInstanceInterceptor.instance;
+         PerJoinpointInterceptor pj1bye = PerJoinpointInterceptor.instance;
+         
+         assertSame(bean1hi, bean1bye);
+         assertSame(pi1hi, pi1bye);
+         assertNotSame(pj1hi, pj1bye);
+   
+         reset(true);
+         result = proxy.sayHi("Me");
+         assertEquals("Hi Me", result);
+         ProxiedBean bean2hi = ProxiedBean.instance;
+         PerInstanceInterceptor pi2hi = PerInstanceInterceptor.instance;
+         PerJoinpointInterceptor pj2hi = PerJoinpointInterceptor.instance;
+         assertNotSame(bean1hi, bean2hi);
+         
+         //FIXME - These must be enabled to start the test
+         assertNotSame(pi2hi, pi1hi);
+         assertNotSame(pj2hi, pj1hi);
+      }
+      finally
+      {
+         log.info(deployer.undeploy());
+      }
+      log.info("======= Done");
    }
    
    private void reset(boolean createNewInstance)
