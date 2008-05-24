b4bc26d458866c8fc96aff336d52f7e428658bf3
Andrew Lee Rubinger <arubinge@redhat.com>
2008-05-24 21:38:41 +0000

    [EJBTHREE-1380] Moved sources to proper package namespace
    git-svn-id: https://svn.jboss.org/repos/jbossas/projects/ejb3/trunk/test@73658 84be2c1e-ba19-0410-b317-a758671a6fc1

--- src/main/java/org/jboss/ejb3/test/mc/bootstrap/EmbeddedTestMcBootstrap.java
+++ src/main/java/org/jboss/ejb3/test/mc/bootstrap/EmbeddedTestMcBootstrap.java
@@ -19,7 +19,7 @@
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
-package org.jboss.test.mc.bootstrap;
+package org.jboss.ejb3.test.mc.bootstrap;
 
 import java.net.URL;
 
