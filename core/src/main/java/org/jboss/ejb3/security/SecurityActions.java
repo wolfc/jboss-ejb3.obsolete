58768dae03fa7f4e4ddd912a51f4fd9314bc39a4
Anil Saldhana <asaldhan@redhat.com>
2008-06-08 02:29:47 +0000

    EJBTHREE-1410: fix package for SecurityContextAssociation
    git-svn-id: https://svn.jboss.org/repos/jbossas/projects/ejb3/trunk@74281 84be2c1e-ba19-0410-b317-a758671a6fc1

--- core/src/main/java/org/jboss/ejb3/security/SecurityActions.java
+++ core/src/main/java/org/jboss/ejb3/security/SecurityActions.java
@@ -37,7 +37,7 @@ import org.jboss.security.SecurityAssociation;
 import org.jboss.security.SecurityContext;
 import org.jboss.security.SecurityContextFactory;
 import org.jboss.security.SubjectInfo;
-import org.jboss.security.plugins.SecurityContextAssociation;
+import org.jboss.security.SecurityContextAssociation;
 
 
 /**
