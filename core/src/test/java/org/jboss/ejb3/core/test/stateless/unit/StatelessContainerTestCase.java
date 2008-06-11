4aacf33b9d535947e857ebf00a401f8823487b36
Carlo de Wolf <cdewolf@redhat.com>
2008-06-11 12:26:33 +0000

    Refactored out an abstract ejb3 test case
    git-svn-id: https://svn.jboss.org/repos/jbossas/projects/ejb3/trunk@74384 84be2c1e-ba19-0410-b317-a758671a6fc1

--- core/src/test/java/org/jboss/ejb3/core/test/stateless/unit/StatelessContainerTestCase.java
+++ core/src/test/java/org/jboss/ejb3/core/test/stateless/unit/StatelessContainerTestCase.java
@@ -23,30 +23,21 @@ package org.jboss.ejb3.core.test.stateless.unit;
 
 import static org.junit.Assert.assertEquals;
 
-import java.net.URL;
 import java.util.Hashtable;
 
 import javax.naming.InitialContext;
 
-import org.jboss.aop.AspectManager;
-import org.jboss.aop.AspectXmlLoader;
 import org.jboss.aop.Domain;
-import org.jboss.aop.DomainDefinition;
-import org.jboss.beans.metadata.plugins.AbstractBeanMetaData;
 import org.jboss.ejb3.Ejb3Deployment;
-import org.jboss.ejb3.InitialContextFactory;
-import org.jboss.ejb3.common.registrar.plugin.mc.Ejb3McRegistrar;
 import org.jboss.ejb3.common.registrar.spi.Ejb3RegistrarLocator;
+import org.jboss.ejb3.core.test.common.AbstractEJB3TestCase;
 import org.jboss.ejb3.core.test.stateless.MyStateless;
 import org.jboss.ejb3.core.test.stateless.MyStatelessBean;
 import org.jboss.ejb3.stateless.StatelessContainer;
 import org.jboss.ejb3.test.cachepassivation.MockDeploymentUnit;
 import org.jboss.ejb3.test.cachepassivation.MockEjb3Deployment;
 import org.jboss.ejb3.test.common.MetaDataHelper;
-import org.jboss.ejb3.test.mc.bootstrap.EmbeddedTestMcBootstrap;
 import org.jboss.metadata.ejb.jboss.JBossSessionBeanMetaData;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 /**
@@ -55,55 +46,8 @@ import org.junit.Test;
  * @author <a href="mailto:carlo.dewolf@jboss.com">Carlo de Wolf</a>
  * @version $Revision: $
  */
-public class StatelessContainerTestCase
+public class StatelessContainerTestCase extends AbstractEJB3TestCase
 {
-   private static EmbeddedTestMcBootstrap bootstrap;
-   
-   @AfterClass
-   public static void afterClass() throws Exception
-   {
-      URL url = Thread.currentThread().getContextClassLoader().getResource("ejb3-interceptors-aop.xml");
-      AspectXmlLoader.undeployXML(url);
-      
-      if(bootstrap != null)
-         bootstrap.shutdown();
-      bootstrap = null;
-   }
-   
-   @BeforeClass
-   public static void beforeClass() throws Exception
-   {
-      // FIXME: weirdness in InitialContextFactory (see EJBTHREE-1097)
-      InitialContextFactory.close(null, null);
-      
-      bootstrap = EmbeddedTestMcBootstrap.createEmbeddedMcBootstrap();
-      
-      // Bind Registrar
-      Ejb3RegistrarLocator.bindRegistrar(new Ejb3McRegistrar(bootstrap.getKernel()));
-      
-      deploy("basicbootstrap-beans.xml");
-      
-      // TODO: AspectDeployment
-      URL url = Thread.currentThread().getContextClassLoader().getResource("ejb3-interceptors-aop.xml");
-      AspectXmlLoader.deployXML(url);
-   }
-   
-   private static void deploy(String resourceName)
-   {
-      URL url = Thread.currentThread().getContextClassLoader().getResource(resourceName);
-      if(url == null)
-         throw new IllegalArgumentException("Can't find a resource named '" + resourceName + "'");
-      assert bootstrap != null : "Can't deploy a resource, bootstrap is null";
-      bootstrap.deploy(url);
-   }
-   
-   private static Domain getDomain(String domainName)
-   {
-      DomainDefinition domainDef = AspectManager.instance().getContainer(domainName);
-      if(domainDef == null) throw new IllegalArgumentException("No such domain '" + domainName + "'");
-      return (Domain) domainDef.getManager();
-   }
-   
    @Test
    public void test1() throws Throwable
    {
@@ -136,6 +80,6 @@ public class StatelessContainerTestCase
       String actual = bean.sayHi("Me");
       assertEquals("Hi Me", actual);
       
-      bootstrap.getKernel().getController().uninstall(containerName);
+      getBootstrap().getKernel().getController().uninstall(containerName);
    }
 }
