b7a2f84bbeb4b0188785881af25cb01fa49aa8a1
Carlo de Wolf <cdewolf@redhat.com>
2008-10-29 14:37:33 +0000

    EJBTHREE-1555: only use method-parameters if specified and make sure we take the most specific interceptor-order
    git-svn-id: https://svn.jboss.org/repos/jbossas/projects/ejb3/trunk@80195 84be2c1e-ba19-0410-b317-a758671a6fc1

---
+++ interceptors/src/test/java/org/jboss/ejb3/test/interceptors/ejbthree1555/unit/CorrectOrderTestCase.java
@@ -0,0 +1,201 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2008, Red Hat Middleware LLC, and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.jboss.ejb3.test.interceptors.ejbthree1555.unit;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import junit.framework.TestCase;
+
+import org.jboss.aop.AspectManager;
+import org.jboss.aspects.common.AOPDeployer;
+import org.jboss.ejb3.interceptors.container.BeanContext;
+import org.jboss.ejb3.interceptors.direct.AbstractDirectContainer;
+import org.jboss.ejb3.interceptors.metadata.BeanInterceptorMetaDataBridge;
+import org.jboss.ejb3.interceptors.metadata.InterceptorComponentMetaDataLoaderFactory;
+import org.jboss.ejb3.interceptors.metadata.InterceptorMetaDataBridge;
+import org.jboss.ejb3.metadata.MetaDataBridge;
+import org.jboss.ejb3.metadata.annotation.AnnotationRepositoryToMetaData;
+import org.jboss.ejb3.test.interceptors.common.CommonAnnotatedInterceptor;
+import org.jboss.ejb3.test.interceptors.common.CommonInterceptor;
+import org.jboss.ejb3.test.interceptors.common.InterceptorChain;
+import org.jboss.ejb3.test.interceptors.ejbthree1555.AnnotatedClassInterceptor;
+import org.jboss.ejb3.test.interceptors.ejbthree1555.AnnotatedMethodInterceptor;
+import org.jboss.ejb3.test.interceptors.ejbthree1555.OrderedSLSB;
+import org.jboss.logging.Logger;
+import org.jboss.metadata.ejb.jboss.JBoss50MetaData;
+import org.jboss.metadata.ejb.jboss.JBossEnterpriseBeanMetaData;
+import org.jboss.metadata.ejb.spec.EjbJar30MetaData;
+import org.jboss.metadata.ejb.spec.InterceptorMetaData;
+import org.jboss.xb.binding.Unmarshaller;
+import org.jboss.xb.binding.UnmarshallerFactory;
+import org.jboss.xb.binding.sunday.unmarshalling.SchemaBinding;
+import org.jboss.xb.binding.sunday.unmarshalling.SchemaBindingResolver;
+import org.jboss.xb.builder.JBossXBBuilder;
+import org.w3c.dom.ls.LSInput;
+
+/**
+ * @author <a href="mailto:cdewolf@redhat.com">Carlo de Wolf</a>
+ * @version $Revision: $
+ */
+public class CorrectOrderTestCase extends TestCase
+{
+   private static final Logger log = Logger.getLogger(CorrectOrderTestCase.class);
+   
+   private MyContainer<OrderedSLSB> container;
+   private BeanContext<OrderedSLSB> bean;
+   
+   private class MyContainer<T> extends AbstractDirectContainer<T, MyContainer<T>>
+   {
+      public MyContainer(String name, String domainName, Class<? extends T> beanClass, JBossEnterpriseBeanMetaData beanMetaData)
+      {
+         super();
+         
+         ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+         AnnotationRepositoryToMetaData annotations = new AnnotationRepositoryToMetaData(beanClass, beanMetaData, name, classLoader);
+         List<MetaDataBridge<InterceptorMetaData>> interceptorBridges = new ArrayList<MetaDataBridge<InterceptorMetaData>>();
+         interceptorBridges.add(new InterceptorMetaDataBridge());
+         annotations.addComponentMetaDataLoaderFactory(new InterceptorComponentMetaDataLoaderFactory(interceptorBridges));
+         annotations.addMetaDataBridge(new BeanInterceptorMetaDataBridge(beanClass, classLoader, beanMetaData));
+         
+         initializeAdvisor(name, getDomain(domainName), beanClass, annotations);
+      }
+
+      public void testAdvisor()
+      {
+         MyContainer<?> container = getAdvisor().getContainer();
+         assertNotNull("container not set in managed object advisor", container);
+         assertTrue(container == this);
+      }
+   }
+   
+   protected static SchemaBindingResolver schemaResolverForClass(final Class<?> root)
+   {
+      return new SchemaBindingResolver()
+      {
+         public String getBaseURI()
+         {
+            return null;
+         }
+
+         public SchemaBinding resolve(String nsUri, String baseURI, String schemaLocation)
+         {
+            return JBossXBBuilder.build(root);
+         }
+
+         public LSInput resolveAsLSInput(String nsUri, String baseUri, String schemaLocation)
+         {
+            return null;
+         }
+
+         public void setBaseURI(String baseURI)
+         {
+         }
+      };
+   }
+
+   // FIXME: use the right jboss-aop.xml
+   AOPDeployer deployer = new AOPDeployer("proxy/jboss-aop.xml");
+   
+   @Override
+   protected void setUp() throws Exception
+   {
+      log.info(deployer.deploy());
+      CommonInterceptor.aroundInvokes = 0;
+      CommonInterceptor.postConstructs = 0;
+      CommonInterceptor.preDestroys = 0;
+      
+      AspectManager.verbose = true;
+      
+      // To make surefire happy
+      Thread.currentThread().setContextClassLoader(OrderedSLSB.class.getClassLoader());
+      
+      // Bootstrap metadata
+      UnmarshallerFactory unmarshallerFactory = UnmarshallerFactory.newInstance();
+      Unmarshaller unmarshaller = unmarshallerFactory.newUnmarshaller();
+      URL url = Thread.currentThread().getContextClassLoader().getResource("ejbthree1555/META-INF/ejb-jar.xml");
+      EjbJar30MetaData metaData = (EjbJar30MetaData) unmarshaller.unmarshal(url.toString(), schemaResolverForClass(EjbJar30MetaData.class));
+      JBoss50MetaData jbossMetaData = new JBoss50MetaData();
+      jbossMetaData.merge(null, metaData);
+      
+      JBossEnterpriseBeanMetaData beanMetaData = jbossMetaData.getEnterpriseBean("OrderedSLSB");
+      assertNotNull(beanMetaData);
+      
+      assertEquals(0, CommonInterceptor.postConstructs);
+      
+      container = new MyContainer<OrderedSLSB>("OrderedSLSB", "Test", OrderedSLSB.class, beanMetaData);
+      container.testAdvisor();
+      
+      bean = container.construct();
+      
+      {
+         Class<?> c[] = { CommonAnnotatedInterceptor.class, AnnotatedClassInterceptor.class };
+         List<Class<?>> expected = Arrays.asList(c);
+         assertEquals(expected, InterceptorChain.getChain());
+         
+         InterceptorChain.clear();
+      }
+   }
+
+   @Override
+   protected void tearDown() throws Exception
+   {
+      container.destroy(bean);
+      InterceptorChain.clear();
+      bean = null;
+      container = null;
+      log.info(deployer.undeploy());
+   }
+   
+   public void testMostSpecificBinding() throws Throwable
+   {
+      container.invoke(bean, "methodWithOwn", "Hello world", 1);
+      
+      {
+         Class<?> c[] = { AnnotatedMethodInterceptor.class };
+         List<Class<?>> expected = Arrays.asList(c);
+         assertEquals(expected, InterceptorChain.getChain());
+         
+         InterceptorChain.clear();
+      }
+      
+      container.invoke(bean, "methodWithOwn");
+      {
+         Class<?> c[] = { AnnotatedClassInterceptor.class, CommonAnnotatedInterceptor.class };
+         List<Class<?>> expected = Arrays.asList(c);
+         assertEquals(expected, InterceptorChain.getChain());
+         
+         InterceptorChain.clear();
+      }
+   }
+   
+   public void testOptionalParameters() throws Throwable
+   {
+      container.invoke(bean, "methodWithParameter", "Hello world");
+      
+      Class<?> c[] = { AnnotatedClassInterceptor.class, CommonAnnotatedInterceptor.class };
+      List<Class<?>> expected = Arrays.asList(c);
+      assertEquals(expected, InterceptorChain.getChain());
+   }
+}
