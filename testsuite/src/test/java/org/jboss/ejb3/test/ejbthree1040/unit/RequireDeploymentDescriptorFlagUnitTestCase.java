4d79e6c2b59814d90c44c7fab1fa9a30bc523a8b
Carlo de Wolf <cdewolf@redhat.com>
2008-12-12 12:33:24 +0000

    EJBTHREE-1241: EJBTHREE-1040 is under development (known-issue)
    git-svn-id: https://svn.jboss.org/repos/jbossas/projects/ejb3/trunk@82243 84be2c1e-ba19-0410-b317-a758671a6fc1

--- testsuite/src/test/java/org/jboss/ejb3/test/ejbthree1040/unit/RequireDeploymentDescriptorFlagUnitTestCase.java
+++ testsuite/src/test/java/org/jboss/ejb3/test/ejbthree1040/unit/RequireDeploymentDescriptorFlagUnitTestCase.java
@@ -23,6 +23,7 @@
 package org.jboss.ejb3.test.ejbthree1040.unit;
 
 import javax.management.Attribute;
+import javax.management.AttributeNotFoundException;
 import javax.management.MBeanServerConnection;
 import javax.management.ObjectName;
 import javax.naming.NameNotFoundException;
@@ -187,7 +188,7 @@ public final class RequireDeploymentDescriptorFlagUnitTestCase extends JBossTest
     */
    public void testDeploySuccessWithRequiredDdAndJbossXml() throws Exception
    {
-      throw new RuntimeException("IMPLEMENT");
+      fail("IMPLEMENT");
    }
 
    /**
@@ -196,7 +197,7 @@ public final class RequireDeploymentDescriptorFlagUnitTestCase extends JBossTest
     */
    public final void testDeploySuccessWithRequiredDdAndEjbJarXml() throws Exception
    {
-      throw new RuntimeException("IMPLEMENT");
+      fail("IMPLEMENT");
    }
 
    // Internal Helper Methods
@@ -208,11 +209,18 @@ public final class RequireDeploymentDescriptorFlagUnitTestCase extends JBossTest
     */
    private void setDeployerToRequireDd(boolean required) throws Exception
    {
-      // Set the flag for the deployer to require a deployment descriptor
-      this.server.setAttribute(this.on, new Attribute(
-            RequireDeploymentDescriptorFlagUnitTestCase.ATTRIBUTE_REQUIRE_DEPLOYMENT_DESCRIPTOR, required
-                  ? Boolean.TRUE
-                  : Boolean.FALSE));
+      try
+      {
+         // Set the flag for the deployer to require a deployment descriptor
+         this.server.setAttribute(this.on, new Attribute(
+               RequireDeploymentDescriptorFlagUnitTestCase.ATTRIBUTE_REQUIRE_DEPLOYMENT_DESCRIPTOR, required
+                     ? Boolean.TRUE
+                     : Boolean.FALSE));
+      }
+      catch(AttributeNotFoundException e)
+      {
+         fail("Attribute not found " + RequireDeploymentDescriptorFlagUnitTestCase.ATTRIBUTE_REQUIRE_DEPLOYMENT_DESCRIPTOR + " on " + this.on);
+      }
    }
 
 }
