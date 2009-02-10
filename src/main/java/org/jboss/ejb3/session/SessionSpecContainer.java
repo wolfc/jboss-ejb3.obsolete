8ad682a01b20dbde9439f2f5f59c224ff160b67e
Andrew Lee Rubinger <arubinge@redhat.com>
2009-02-10 02:45:37 +0000

    [EJBTHREE-1641] Resolve "ejbcontext" regressions from removal of old proxy impl
    git-svn-id: https://svn.jboss.org/repos/jbossas/projects/ejb3/trunk/core@84025 84be2c1e-ba19-0410-b317-a758671a6fc1

--- src/main/java/org/jboss/ejb3/session/SessionSpecContainer.java
+++ src/main/java/org/jboss/ejb3/session/SessionSpecContainer.java
@@ -16,8 +16,6 @@ import org.jboss.aop.MethodInfo;
 import org.jboss.aop.util.MethodHashing;
 import org.jboss.ejb3.Ejb3Deployment;
 import org.jboss.ejb3.ThreadLocalStack;
-import org.jboss.ejb3.annotation.LocalBinding;
-import org.jboss.ejb3.annotation.RemoteBinding;
 import org.jboss.ejb3.common.lang.SerializableMethod;
 import org.jboss.ejb3.common.registrar.spi.Ejb3RegistrarLocator;
 import org.jboss.ejb3.proxy.container.InvokableContext;
@@ -68,52 +66,6 @@ public abstract class SessionSpecContainer extends SessionContainer implements I
    {
       super(cl, beanClassName, ejbName, domain, ctxProperties, deployment, beanMetaData);
    }
-
-//   /**
-//    * Create a remote proxy (EJBObject) for an enterprise bean identified by id
-//    * 
-//    * @param id
-//    * @return
-//    * @throws Exception
-//    */
-//   public Object createProxyRemoteEjb21() throws Exception
-//   {
-//      RemoteBinding binding = this.getRemoteBinding();
-//      return this.createProxyRemoteEjb21(binding);
-//   }
-//
-//   /**
-//    * Create a remote proxy (EJBObject) for an enterprise bean identified by id on a given binding
-//    * 
-//    * @param id
-//    * @param binding
-//    * @return
-//    * @throws Exception
-//    */
-//   public abstract Object createProxyRemoteEjb21(RemoteBinding binding) throws Exception;
-//
-//   /**
-//    * Create a local proxy (EJBLocalObject) for an enterprise bean identified by id
-//    * 
-//    * @param id
-//    * @return
-//    * @throws Exception
-//    */
-//   public Object createProxyLocalEjb21() throws Exception
-//   {
-//      LocalBinding binding = this.getAnnotation(LocalBinding.class);
-//      return this.createProxyLocalEjb21(binding);
-//   }
-//
-//   /**
-//    * Create a local proxy (EJBLocalObject) for an enterprise bean identified by id, with
-//    * the specified LocalBinding
-//    * 
-//    * @param id
-//    * @return
-//    * @throws Exception
-//    */
-//   public abstract Object createProxyLocalEjb21(LocalBinding binding) throws Exception;
 
    /**
     * Invokes the method described by the specified serializable method
