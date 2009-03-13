8985d5744f250ec3a5028d5729017abfe25155fb
Carlo de Wolf <cdewolf@redhat.com>
2009-03-13 13:13:30 +0000

    EJBTHREE-1771: a transition component until AS no longer depends on any proxy stuff
    git-svn-id: https://svn.jboss.org/repos/jbossas/projects/ejb3/trunk/proxy@85822 84be2c1e-ba19-0410-b317-a758671a6fc1

--- src/main/java/org/jboss/ejb3/proxy/jndiregistrar/JndiServiceRegistrar.java
+++ src/main/java/org/jboss/ejb3/proxy/jndiregistrar/JndiServiceRegistrar.java
@@ -1,9 +1,9 @@
 /*
  * JBoss, Home of Professional Open Source.
- * Copyright 2008, Red Hat Middleware LLC, and individual contributors
+ * Copyright 2009, Red Hat Middleware LLC, and individual contributors
  * as indicated by the @author tags. See the copyright.txt file in the
  * distribution for a full listing of individual contributors.
-  *
+ *
  * This is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation; either version 2.1 of
@@ -21,110 +21,18 @@
  */
 package org.jboss.ejb3.proxy.jndiregistrar;
 
-import org.jboss.aop.Advisor;
-import org.jboss.aop.Dispatcher;
-import org.jboss.ejb3.proxy.factory.ProxyFactory;
-import org.jboss.ejb3.proxy.factory.session.service.ServiceLocalProxyFactory;
-import org.jboss.ejb3.proxy.factory.session.service.ServiceRemoteProxyFactory;
-import org.jboss.logging.Logger;
-import org.jboss.metadata.ejb.jboss.JBossServiceBeanMetaData;
-import org.jboss.metadata.ejb.jboss.JBossSessionBeanMetaData;
-
 /**
- * JndiServiceRegistrar
- * 
- * Responsible for binding of ObjectFactories and
- * creation/registration of associated ProxyFactories, 
- * centralizing operations for @Service Implementations
- *
- * @author <a href="mailto:andrew.rubinger@jboss.org">ALR</a>
+ * @author <a href="mailto:cdewolf@redhat.com">Carlo de Wolf</a>
  * @version $Revision: $
  */
-public class JndiServiceRegistrar extends JndiSessionRegistrarBase
+@Deprecated
+public class JndiServiceRegistrar extends org.jboss.ejb3.proxy.impl.jndiregistrar.JndiServiceRegistrar
 {
-
-   // --------------------------------------------------------------------------------||
-   // Class Members ------------------------------------------------------------------||
-   // --------------------------------------------------------------------------------||
-
-   private static final Logger log = Logger.getLogger(JndiServiceRegistrar.class);
-
-   // --------------------------------------------------------------------------------||
-   // Constructor --------------------------------------------------------------------||
-   // --------------------------------------------------------------------------------||
-
    /**
-    * Creates a JNDI Registrar from the specified configuration properties, none of
-    * which may be null.
-    * 
-    * @param serviceProxyObjectFactoryType String representation of the JNDI Object Factory to use for @Service
+    * @param serviceProxyObjectFactoryType
     */
    public JndiServiceRegistrar(String serviceProxyObjectFactoryType)
    {
       super(serviceProxyObjectFactoryType);
    }
-
-   // --------------------------------------------------------------------------------||
-   // Required Implementations -------------------------------------------------------||
-   // --------------------------------------------------------------------------------||
-
-   /**
-    * Creates and returns a new local proxy factory for this @Service
-    * 
-    * @param name The unique name for the ProxyFactory
-    * @param containerName The name of the Container upon which Proxies 
-    *   from the returned ProxyFactory will invoke
-    * @param containerGuid The globally-unique name of the container
-    * @param smd The metadata representing this SFSB
-    * @param cl The ClassLoader for this EJB Container
-    * @param advisor The Advisor for proxies created by this factory
-    */
-   @Override
-   protected ProxyFactory createLocalProxyFactory(final String name, final String containerName,
-         final String containerGuid, final JBossSessionBeanMetaData smd, final ClassLoader cl, final Advisor advisor)
-   {
-      assert (smd instanceof JBossServiceBeanMetaData) : "Specified metadata was not of expected type "
-            + JBossServiceBeanMetaData.class.getSimpleName();
-      JBossServiceBeanMetaData serviceMd = (JBossServiceBeanMetaData) smd;
-      return new ServiceLocalProxyFactory(name, containerName, containerGuid, serviceMd, cl, advisor);
-   }
-
-   /**
-    * Creates and returns a new remote proxy factory for this SFSB
-    * 
-    * @param name The unique name for the ProxyFactory
-    * @param containerName The name of the Container upon which Proxies 
-    *   from the returned ProxyFactory will invoke
-    * @param containerGuid The globally-unique name of the container
-    * @param smd The metadata representing this SFSB
-    * @param cl The ClassLoader for this EJB Container
-    * @param url The URL to use for Remoting
-    * @param advisor The Advisor for proxies created by this factory
-    * @param interceptorStackName The name of the client-side interceptor stack to use.
-    *       If null the default will apply.
-    */
-   @Override
-   protected ProxyFactory createRemoteProxyFactory(final String name, final String containerName,
-         final String containerGuid, final JBossSessionBeanMetaData smd, final ClassLoader cl, final String url,
-         final Advisor advisor, final String interceptorStackName)
-   {
-      // Ensure metadata is of expected type
-      assert (smd instanceof JBossServiceBeanMetaData) : "Specified metadata was not of expected type "
-            + JBossServiceBeanMetaData.class.getSimpleName();
-
-      // Cast
-      JBossServiceBeanMetaData serviceMd = (JBossServiceBeanMetaData) smd;
-
-      // Create
-      ProxyFactory factory = new ServiceRemoteProxyFactory(name, containerName, containerGuid, serviceMd, cl, url,
-            advisor, interceptorStackName);
-
-      // Register with Remoting
-      log.debug("Registering with Remoting Dispatcher under name \"" + factory.getName() + "\": " + factory);
-      Dispatcher.singleton.registerTarget(factory.getName(), factory);
-
-      // Return
-      return factory;
-   }
-
 }
--- src/main/java/org/jboss/ejb3/proxy/objectfactory/session/stateful/StatefulSessionProxyObjectFactory.java
+++ src/main/java/org/jboss/ejb3/proxy/objectfactory/session/stateful/StatefulSessionProxyObjectFactory.java
@@ -1,9 +1,9 @@
 /*
  * JBoss, Home of Professional Open Source.
- * Copyright 2008, Red Hat Middleware LLC, and individual contributors
+ * Copyright 2009, Red Hat Middleware LLC, and individual contributors
  * as indicated by the @author tags. See the copyright.txt file in the
  * distribution for a full listing of individual contributors.
-  *
+ *
  * This is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation; either version 2.1 of
@@ -21,50 +21,12 @@
  */
 package org.jboss.ejb3.proxy.objectfactory.session.stateful;
 
-import java.util.List;
-import java.util.Map;
-
-import javax.naming.Name;
-
-import org.jboss.ejb3.proxy.factory.ProxyFactory;
-import org.jboss.ejb3.proxy.objectfactory.session.SessionProxyObjectFactory;
-
 /**
- * StatefulSessionProxyObjectFactory
- * 
- * A JNDI ObjectFactory responsible for returning the
- * appropriate SFSB Proxy upon lookup
- *
- * @author <a href="mailto:andrew.rubinger@jboss.org">ALR</a>
+ * @author <a href="mailto:cdewolf@redhat.com">Carlo de Wolf</a>
  * @version $Revision: $
  */
-public class StatefulSessionProxyObjectFactory extends SessionProxyObjectFactory
+@Deprecated
+public class StatefulSessionProxyObjectFactory extends org.jboss.ejb3.proxy.impl.objectfactory.session.stateful.StatefulSessionProxyObjectFactory
 {
-   // --------------------------------------------------------------------------------||
-   // Class Members  -----------------------------------------------------------------||
-   // --------------------------------------------------------------------------------||
-
    private static final long serialVersionUID = 1L;
-
-   // --------------------------------------------------------------------------------||
-   // Required Implementations -------------------------------------------------------||
-   // --------------------------------------------------------------------------------||
-
-   /**
-    * SFSB Object Factories must always create a new SFSB Proxy
-    * 
-    * @param proxyFactory The ProxyFactory to use
-    * @param name The JNDI name looked up
-    * @param referenceAddresses
-    */
-   @Override
-   protected Object getProxy(ProxyFactory proxyFactory, Name name, Map<String, List<String>> referenceAddresses)
-   {
-      // Create a new Proxy Instance
-      Object proxy = this.createProxy(proxyFactory, name, referenceAddresses);
-
-      // Return the Proxy
-      return proxy;
-   }
-
 }
