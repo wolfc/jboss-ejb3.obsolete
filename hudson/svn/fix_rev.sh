6346e6f69fac4c55112928aaf39cdcb7869bb4fb
Carlo de Wolf <cdewolf@redhat.com>
2009-04-03 07:16:04 +0000

    workaround for http://subversion.tigris.org/issues/show_bug.cgi?id=3194
    git-svn-id: https://svn.jboss.org/repos/jbossas/projects/ejb3/trunk@86727 84be2c1e-ba19-0410-b317-a758671a6fc1

---
+++ hudson/svn/fix_rev.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+# vim:ts=3:sw=3:expandtab:
+set -e
+#set -x
+
+# Fix the svn:date of a revision in a SVN mirror
+# http://subversion.tigris.org/issues/show_bug.cgi?id=3194
+
+if [ $# != 2 ]; then
+   echo 1>&2 "Usage: $0 <repos> <rev>"
+   exit 1
+fi
+
+REPOS="$1"
+REV="$2"
+
+DATE=`svn pg -r $REV --revprop svn:date $REPOS`
+
+if [ -n "$DATE" ]; then
+   echo "$REV already has a date"
+   exit 0
+fi
+
+PREV_REV=$REV
+while [ -z "$DATE" ]; do
+   PREV_REV=`expr $PREV_REV - 1`
+   if [ $PREV_REV == -1 ]; then
+      echo 1>&2 "can't find a previous date for $REV"
+      exit 1
+   fi
+   DATE=`svn pg -r $PREV_REV --revprop svn:date $REPOS`
+done
+echo $DATE
+
+svn ps -r $REV --revprop svn:date $DATE $REPOS
