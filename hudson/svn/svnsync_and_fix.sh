93458785acbbda42a2e95e12ac0642bdd57c6d08
Carlo de Wolf <cdewolf@redhat.com>
2009-04-29 10:48:40 +0000

    Using a sync and fix instead of post-commit hook
    git-svn-id: https://svn.jboss.org/repos/jbossas/projects/ejb3/trunk@87988 84be2c1e-ba19-0410-b317-a758671a6fc1

---
+++ hudson/svn/svnsync_and_fix.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+set -ex
+
+if [ $# -lt 1 ]; then
+   echo 1>&2 "Usage: $0 <path>"
+   exit 1
+fi
+
+REPO_PATH=$1
+URL=file://$REPO_PATH
+
+PREV_REV=`svn info $URL | grep "^Revision:" | cut -c11-`
+PREV_REV=`expr $PREV_REV + 1`
+/usr/bin/svnsync synchronize $URL
+CURRENT_REV=`svn info $URL | grep "^Revision:" | cut -c11-`
+
+for rev in $(seq $PREV_REV $CURRENT_REV); do
+   ./fix_rev.sh $REPO_PATH $rev
+done
